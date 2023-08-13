163dfe5e649e38848ca98025e01895894c4899ff
Kunal Raju Hulke <99308881+krh1102002@users.noreply.github.com>
2023-08-13 15:41:28 +0530

    deleting files due to internal errors

--- server/Config/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt=require("jsonwebtoken");
-
-//jwt takes 3 things:1.value provided , 2.time, 3. expiration time
-const generateToken=(id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:"30d"
-    })
-}
-module.exports=generateToken;
--- server/Routers/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { loginController, registerController } = require('../Controllers/userController');
-
-const router=express.Router()
-router.post("/login",loginController);
-router.post("/register",registerController);
-
-module.exports=router;
